3f1bd01148871fe3b97b37cc0300d1683f5e791a
Kyle Devine <devinenoise@gmail.com>
2023-12-29 06:53:34 -0800

    add content type of faqs

---
+++ src/content/config.ts
@@ -0,0 +1,11 @@
+import { defineCollection, z } from 'astro:content';
+
+const faqs = defineCollection({
+  type: 'data',
+  schema: z.object({
+    question: z.string(),
+    answer: z.string()
+  })
+});
+
+export const collections = { faqs: faqs };
